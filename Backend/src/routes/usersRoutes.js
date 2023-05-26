2b9f96c9554f6ca88885526784c642a4b117d96b
ramandio <69742987+ramandio@users.noreply.github.com>
2023-05-27 03:06:10 +0700

    Init React App + Daisy UI

--- Backend/src/routes/usersRoutes.js
+++ Backend/src/routes/usersRoutes.js
@@ -7,15 +7,15 @@ const usersController = require('../controllers/usersControllers');
 router.post('/register', usersController.registerUser);
 
 // Login route
-// router.post('/login', usersController.loginUser);
+router.post('/login', usersController.loginUser);
 
 // // Route to get user by ID
-// router.get('/get/:id', usersController.getUserById);
+router.get('/get/:id', usersController.getUserById);
 
 // // Route to update user by ID
-// router.put('/update/:id', usersController.updateUserById);
+router.put('/update/:id', usersController.updateUserById);
 
 // // Route to delete user by ID
-// router.delete('/delete/:id', usersController.deleteUserById);
+router.delete('/delete/:id', usersController.deleteUserById);
 
 module.exports = router;
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <><h1 className="text-3xl font-bold underline text-blue-500">
+      Hello world!
+    </h1><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button></>
+    
+  );
+}
+
+export default App;
